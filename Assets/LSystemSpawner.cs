6e300fe71f9d5e1aa958986e1e69b2adeb9fddab
MariusKM <m.morhard@hotmail.de>
2020-03-28 20:02:13 +0100

    fixed spawner Positions

--- Assets/LSystemSpawner.cs
+++ Assets/LSystemSpawner.cs
@@ -9,7 +9,7 @@ public class LSystemSpawner : MonoBehaviour
     public int amountX, amountZ;
     Bounds spawnBounds;
 
-    GameObject[] generatedObjects;
+    public GameObject[] generatedObjects;
     public LSystemsGenerator[] generatorTemplate;
 
 
@@ -19,8 +19,7 @@ public class LSystemSpawner : MonoBehaviour
         spawnArea = GetComponent<BoxCollider>();
         spawnArea.isTrigger = true;
         spawnBounds = spawnArea.bounds;
-        generatedObjects = new GameObject[amountX * amountZ];
-
+        generatedObjects = new GameObject[(amountX) * (amountZ)];
         Spawn();
     }
 
@@ -30,7 +29,6 @@ public class LSystemSpawner : MonoBehaviour
         for (int i = 0; i < generatedObjects.Length; i++)
         {
             int indexer = Random.Range(0, generatorTemplate.Length);
-            Debug.Log(indexer);
             generatorTemplate[indexer].Init();
             generatorTemplate[indexer].initColliders();
             generatorTemplate[indexer].setUpPhysics();
@@ -71,19 +69,22 @@ public class LSystemSpawner : MonoBehaviour
 
         float stepX = sizeX / (amountX-1);
         float stepZ = sizeZ / (amountZ-1);
+        Debug.Log(stepX);
+        Debug.Log(stepZ);
 
-        Vector3 bottomLeft = new Vector3(-sizeX / 2, 0, -sizeZ/2 );
-
-        for (int z = 0; z < amountZ; z++)
+        Vector3 bottomLeft = new Vector3(-sizeX / 2.0f, 0, -sizeZ/2.0f );
+        for (int i = 0, z = 0; z <amountZ; z++)
         {
-            for (int x = 0; x < amountX; x++)
+            for (int x = 0; x <amountX; x++,i++)
             {
                 Vector3 spawnPos = bottomLeft + new Vector3(stepX * x, 0, stepZ * z);
                 Debug.Log(spawnPos);
-                Debug.Log(z * amountZ + x);
-              //  Debug.Log(stepZ * z);
-               // Debug.Log((stepX * x));
-                generatedObjects[z*amountZ+ x].transform.localPosition = spawnPos;
+                Debug.Log(x);
+                Debug.Log(z);
+                Debug.Log(z * amountZ + x+z);
+     
+                generatedObjects[i].transform.localPosition = spawnPos;
+            
             }
         }
 
